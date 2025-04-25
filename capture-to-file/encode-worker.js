75d5c59876d7c19b6b153208b0d07a468bb08d88
Hendri <hendri.wang88@gmail.com>
2025-04-25 17:36:51 +0200

    change to h265

--- capture-to-file/encode-worker.js
+++ capture-to-file/encode-worker.js
@@ -56,7 +56,7 @@ async function startRecording(fileHandle, frameStream, trackSettings) {
     hardwareAcceleration: 'prefer-hardware',
     bitrate: 10e6,
     framerate: 30,
-    scalabilityMode: 'L1T1',
+    // scalabilityMode: 'L1T1',
     alpha: 'discard',
     bitrateMode: 'constant',
     latencyMode: 'realtime',
